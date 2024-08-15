be4d9c8ca3255c5235699ac9e1028c2d63ddd4b0
manuel-morales-a <manu.morales.alvarado@gmail.com>
2024-08-15 11:51:06 -0400

    Add main and requirements

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+main.py
+
+Main module to execute the INSPIRE LA analysis.
+
+Note: some functions will be taken from the Jupyter notebooks of the `exploration` branch of the project.
+Date: 15/08/2024
+"""
+
+
+def main():
+    # Your code here
+    pass
+
+
+if __name__ == "__main__":
+    main()
