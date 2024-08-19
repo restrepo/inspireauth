a93521aed70d92b0a95aacfa1e8e230d4ae7632f
manuel-morales-a <manu.morales.alvarado@gmail.com>
2024-08-19 17:08:47 -0400

    Complete structure

--- main.py
+++ main.py
@@ -4,13 +4,30 @@ main.py
 Main module to execute the INSPIRE LA analysis.
 
 Note: some functions will be taken from the Jupyter notebooks of the `exploration` branch of the project.
-Date: 15/08/2024
+Date: 19/08/2024
 """
+
+from scripts.plotting_publications import (
+    plot_articles_per_year,
+    plot_combined_papers_per_year,
+)
 
 
 def main():
-    # Your code here
-    pass
+    """
+    Main function to execute the analysis of the INSPIRE LA dataset.
+    """
+    # Example usage of the plotting functions
+    countries = ["Chile", "Mexico"]
+
+    # Plot individual articles per year for a specific country
+    for entry in countries:
+        plot_articles_per_year(country=entry, save=True)
+
+    # Plot combined papers per year for multiple countries
+    plot_combined_papers_per_year(countries, save=True)
+
+    print("Analysis complete!")
 
 
 if __name__ == "__main__":
