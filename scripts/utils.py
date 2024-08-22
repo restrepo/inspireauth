4dff0235ad1e3279434e2f92daca258c1367c2ca
manuel-morales-a <manu.morales.alvarado@gmail.com>
2024-08-22 17:34:33 -0400

    Added utils module

---
+++ scripts/utils.py
@@ -0,0 +1,31 @@
+"""
+scripts.utils.py
+
+Module containing utils to analyse the INSPIRE LA dataset.
+
+Note: some functions will be taken from the Jupyter notebooks of the `exploration` branch of the project.
+Date: 22/08/2024
+"""
+
+# 19 Latin American countries indexed in the database
+LA_countries = [
+    "Argentina",
+    "Bolivia",
+    "Brazil",
+    "Chile",
+    "Colombia",
+    "Costa Rica",
+    "Cuba",
+    "Dominican Republic",  # new
+    "Ecuador",
+    "El Salvador",  # new
+    "Guatemala",
+    "Honduras",
+    "Mexico",
+    "Panama",  # new
+    "Paraguay",
+    "Peru",
+    "Puerto Rico",  # new
+    "Uruguay",
+    "Venezuela",
+]
